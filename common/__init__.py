91ce063e3648e8b9f7acb08669031577b73d3c55
KnifeyMoloko <maciej.a.cisowski@gmail.com>
2020-05-01 13:26:34 +0200

    travis setup, simple tests for common, some cleanup

---
+++ common/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+GUS_ROOT_DIR = Path.cwd().parent
+GUS_CONFIG_DIR = GUS_ROOT_DIR / "config"
---
+++ tests/tests_common.py
@@ -0,0 +1,32 @@
+"""
+Test for the common package of GUS.
+Author: Maciej Cisowski (KnifeyMoloko)
+"""
+from assertpy import assert_that, soft_assertions
+from common.helpers import get_env_vars, get_api_key, \
+    get_env_type, get_config
+from ruamel.yaml.comments import CommentedMap
+
+
+def test_env_vars_are_not_empty():
+    assert_that(get_env_vars()).is_not_empty()
+
+
+def test_get_api_key():
+    acutal = get_api_key()
+    with soft_assertions():
+        assert_that(acutal).is_not_none()
+        assert_that(acutal).is_type_of(str)
+        assert_that(len(acutal)).is_greater_than(1)
+
+
+def test_get_env_type():
+    assert_that(get_env_type()).is_equal_to("test")
+
+
+def test_get_config():
+    config = get_config()
+    with soft_assertions():
+        assert_that(config).is_not_empty()
+        assert_that(config).is_type_of(CommentedMap)
+        assert_that(config['version']).is_equal_to("0.0.1")
